cbc7084a17f822652f955c5c693f638c0852a1c7
Artem Sidorenko <artem.sydorenko@yalantis.com>
2015-10-13 15:54:50 +0300

    Add license information into source code

--- PullToRefresh/PullToRefresh.h
+++ PullToRefresh/PullToRefresh.h
@@ -1,9 +1,9 @@
 //
-//  PullToRefresh.h
-//  PullToRefresh
-//
 //  Created by Anastasiya Gorban on 5/22/15.
 //  Copyright (c) 2015 Yalantis. All rights reserved.
+//
+//  Licensed under the MIT license: http://opensource.org/licenses/MIT
+//  Latest version can be found at https://github.com/Yalantis/PullToRefresh
 //
 
 #import <UIKit/UIKit.h>
