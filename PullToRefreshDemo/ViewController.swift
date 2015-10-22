8a7f7cd28d1990232cf67d47cedca40b2337e36f
roman.sherbakov <roman.sherbakov@yalantis.com>
2015-10-22 12:39:00 +0300

    [Fix] fix indicator position after device rotate

--- PullToRefreshDemo/ViewController.swift
+++ PullToRefreshDemo/ViewController.swift
@@ -14,22 +14,46 @@ class ViewController: UIViewController {
     @IBOutlet
     private var tableView: UITableView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "deviceRotated", name: UIDeviceOrientationDidChangeNotification, object: nil)
+    }
+    
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
 
-        tableView.addPullToRefresh(PullToRefresh(), action: { [weak self] in
-            let delayTime = dispatch_time(DISPATCH_TIME_NOW,
-                Int64(2 * Double(NSEC_PER_SEC)))
-            dispatch_after(delayTime, dispatch_get_main_queue()) {
-                self?.tableView.endRefreshing()
-            }
-        })
+        createPullToRefresh ()
     }
     
     override func viewDidDisappear(animated: Bool) {
         super.viewDidDisappear(animated)
         
         tableView.removePullToRefresh(tableView.pullToRefresh!)
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIDeviceOrientationDidChangeNotification, object: nil)
+    }
+    
+    func deviceRotated() {
+        if(UIDeviceOrientationIsLandscape(UIDevice.currentDevice().orientation)) {
+            createPullToRefresh ()
+        }
+        if(UIDeviceOrientationIsPortrait(UIDevice.currentDevice().orientation)) {
+            createPullToRefresh ()
+        }
+        
+    }
+    
+    private func createPullToRefresh () {
+        tableView.addPullToRefresh(PullToRefresh(), action: { [weak self] in
+            let delayTime = dispatch_time(DISPATCH_TIME_NOW,
+                Int64(2 * Double(NSEC_PER_SEC)))
+            dispatch_after(delayTime, dispatch_get_main_queue()) {
+                self?.tableView.endRefreshing()
+            }
+            })
     }
     
     @IBAction
